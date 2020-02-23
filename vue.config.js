39b9cf5cf9d3cb20dd17aad8dcef71604ccdfbf2
Gabe Cook <gabe565@gmail.com>
2020-02-23 01:05:28 -0600

    :wrench: Clean up config to change generated title and add PWA title config

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   chainWebpack: (config) => {
-    config
-      .plugin('html')
-      .tap((args) => {
-        args[0].title = 'Relax Sounds';
-        return args;
-      });
+    config.plugin('html').tap((args) => {
+      args[0].title = 'Relax Sounds';
+      return args;
+    });
+  },
+  pwa: {
+    name: 'Relax Sounds',
   },
   transpileDependencies: [
     'vuetify',
