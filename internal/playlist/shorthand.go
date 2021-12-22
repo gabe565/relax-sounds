cc343522ada8b0704031734bff14add7b77c1ae6
Gabe Cook <gabe565@gmail.com>
2021-12-22 17:32:43 -0600

    :zap: Convert ShorthandEntry to array

--- internal/playlist/shorthand.go
+++ internal/playlist/shorthand.go
@@ -7,17 +7,13 @@ import (
 
 var ErrInvalidShorthand = errors.New("invalid shorthand")
 
-type ShorthandEntry []interface{}
+type ShorthandEntry [2]interface{}
 
 type Shorthand []ShorthandEntry
 
 func (shorthand Shorthand) ToPlaylist() (Playlist, error) {
 	playlist := make(Playlist, 0, len(shorthand))
 	for _, value := range shorthand {
-		if len(value) != 2 {
-			// Shorthand is not a tuple
-			return playlist, ErrInvalidShorthand
-		}
 		playlist = append(playlist, Entry{
 			Key:    fmt.Sprintf("%v", value[0]),
 			Volume: value[1].(float64) - 1,
