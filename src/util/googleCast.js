a78a940c0cf4e1e6d27fad3193269d6d61eaff80
Gabe Cook <gabe565@gmail.com>
2021-12-22 14:37:27 -0600

    :sparkles: Add cast metadata

--- src/util/googleCast.js
+++ src/util/googleCast.js
@@ -1,4 +1,5 @@
 import { encode } from './shareUrl';
+import soundConfig from '../data/sounds.json';
 
 export function getCastSession() {
   return window.cast.framework.CastContext.getInstance().getCurrentSession();
@@ -8,6 +9,16 @@ export function castPlaylist(castSession, playlist) {
   const { sounds } = encode(playlist);
   const url = `${window.location.origin}/mix/${sounds}`;
   const mediaInfo = new window.chrome.cast.media.MediaInfo(url, 'music');
+
+  mediaInfo.metadata = new window.chrome.cast.media.MusicTrackMediaMetadata();
+  const playlistIds = playlist.sounds.map((s) => s.id);
+  mediaInfo.metadata.title = soundConfig
+    .filter((s) => playlistIds.includes(s.id))
+    .map((s) => s.name)
+    .sort((a, b) => a.localeCompare(b))
+    .join(', ');
+  mediaInfo.metadata.artist = 'Relax Sounds';
+
   const request = new window.chrome.cast.media.LoadRequest(mediaInfo);
   return castSession.loadMedia(request);
 }
