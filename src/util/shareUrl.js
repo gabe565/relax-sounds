bc5012df9a8233a3e9a262c76f2a64315cfc89a9
Gabe Cook <gabe565@gmail.com>
2021-12-26 17:45:39 -0600

    :sparkles: Create functions to encode/decode sounds without requiring playlist name

--- src/util/shareUrl.js
+++ src/util/shareUrl.js
@@ -1,17 +1,25 @@
 import base64 from 'base64-url';
 
+export function encodeSounds(sounds) {
+  return base64.encode(JSON.stringify(
+    sounds.map((sound) => [sound.id, Math.round(sound.volume * 1000) / 1000]),
+  ));
+}
+
 export function encode(playlist) {
   const name = encodeURIComponent(playlist.name).replace(/%20/g, '+');
-  const sounds = base64.encode(JSON.stringify(
-    playlist.sounds.map((sound) => [sound.id, Math.round(sound.volume * 1000) / 1000]),
-  ));
+  const sounds = encodeSounds(playlist.sounds);
   return { name, sounds };
+}
+
+export function decodeSounds(param) {
+  return JSON.parse(base64.decode(param)).map(
+    (song) => ({ id: song[0], volume: song[1] }),
+  );
 }
 
 export function decode(params) {
   const name = params.name.replace(/\+/g, ' ');
-  const sounds = JSON.parse(base64.decode(params.songs)).map(
-    (song) => ({ id: song[0], volume: song[1] }),
-  );
+  const sounds = decodeSounds(params.songs);
   return { name, sounds };
 }
