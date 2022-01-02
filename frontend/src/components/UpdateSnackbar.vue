730919e9af9c9d2bc773647d1e8fbe4b05127df5
Gabe Cook <gabe565@gmail.com>
2022-01-02 01:02:13 -0600

    :lipstick: Tweak "new version" snackbar

--- frontend/src/components/UpdateSnackbar.vue
+++ frontend/src/components/UpdateSnackbar.vue
@@ -2,20 +2,23 @@
     <v-snackbar
       v-model="show"
       :timeout="timeout"
-      bottom left class="pa-4" color="deep-orange darken-4"
+      bottom right
     >
       New version available!
-      <v-spacer/>
-      <v-btn icon @click.native="refreshApp">
-        <v-icon>fal fa-sync</v-icon>
-      </v-btn>
-      <v-btn icon @click="show = false">
-        <v-icon>$close</v-icon>
-      </v-btn>
+      <template #action="{ attrs }">
+        <v-btn v-bind="attrs" @click.native="refreshApp" text color="primary">
+          Refresh
+        </v-btn>
+        <v-btn icon @click="show = false">
+          <v-icon>$close</v-icon>
+        </v-btn>
+      </template>
     </v-snackbar>
 </template>
 
 <script>
+import { wait } from '../util/helpers';
+
 export default {
   name: 'UpdateSnackbar',
 
@@ -38,12 +41,13 @@ export default {
   },
 
   methods: {
-    showRefreshUI(e) {
+    async showRefreshUI(e) {
       // Display a snackbar inviting the user to refresh/reload the app due
       // to an app update being available.
       // The new service worker is installed, but not yet active.
       // Store the ServiceWorkerRegistration instance for later use.
       this.registration = e.detail;
+      await wait(1000);
       this.show = true;
     },
 
