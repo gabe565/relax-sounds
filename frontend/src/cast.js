f90667a9e5336a29a6e7b0f0255a4b2758543d00
Gabe Cook <gabe565@gmail.com>
2022-01-23 21:37:33 -0600

    :rotating_light: Remove eslint overrides

--- frontend/src/cast.js
+++ frontend/src/cast.js
@@ -1,7 +1,5 @@
 import store from './plugins/store/main';
 
-// eslint-disable-next-line no-underscore-dangle
-// eslint-disable-next-line dot-notation
 window.__onGCastApiAvailable = (isAvailable) => {
   if (isAvailable) {
     store.dispatch('player/initializeCastApi');
