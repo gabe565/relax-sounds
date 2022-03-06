74f97df459ef95bde62d3d0f46c39a581b12a09e
Gabe Cook <gabe565@gmail.com>
2022-03-06 01:29:25 -0600

    :recycle: Add NewStreamer func

--- internal/stream/streamer.go
+++ internal/stream/streamer.go
@@ -2,6 +2,9 @@ package stream
 
 import (
 	"github.com/faiface/beep"
+	"github.com/faiface/beep/effects"
+	"github.com/gabe565/relax-sounds/internal/preset"
+	"io/fs"
 )
 
 type Streamer struct {
@@ -15,4 +18,37 @@ func (s Streamer) Close() error {
 		return s.Closer.Close()
 	}
 	return nil
+}
+
+func NewStreamer(fsys fs.FS, entry preset.Track) (streamer Streamer, err error) {
+	rawFile, err := fsys.Open(entry.Path())
+	if err != nil {
+		return streamer, err
+	}
+	f := File{File: rawFile}
+
+	closer, format, err := f.Decode()
+	if err != nil {
+		return streamer, err
+	}
+	streamer.Closer = closer
+	streamer.Format = format
+
+	beepStreamer := beep.Loop(-1, closer)
+
+	if entry.Volume != 1 {
+		beepStreamer = &effects.Volume{
+			Streamer: beepStreamer,
+			Base:     10,
+			Volume:   entry.Volume - 1,
+		}
+	}
+
+	if format.NumChannels < 2 {
+		// Fix mono streams playing at 2x speed
+		beepStreamer = beep.ResampleRatio(3, 0.5, beepStreamer)
+	}
+	streamer.Streamer = beepStreamer
+
+	return streamer, nil
 }
--- internal/stream/streams.go
+++ internal/stream/streams.go
@@ -2,7 +2,6 @@ package stream
 
 import (
 	"github.com/faiface/beep"
-	"github.com/faiface/beep/effects"
 	"github.com/gabe565/relax-sounds/internal/preset"
 	"io/fs"
 	"sync"
@@ -17,40 +16,15 @@ func (stream Streams) Close() error {
 	return nil
 }
 
-func (stream *Streams) Add(dataDir fs.FS, entry preset.Track, mu *sync.Mutex) error {
-	rawFile, err := dataDir.Open(entry.Path())
+func (stream *Streams) Add(fsys fs.FS, entry preset.Track, mu *sync.Mutex) error {
+	streamer, err := NewStreamer(fsys, entry)
 	if err != nil {
 		return err
-	}
-	f := File{File: rawFile}
-
-	closer, format, err := f.Decode()
-	if err != nil {
-		return err
-	}
-
-	streamer := beep.Loop(-1, closer)
-
-	if entry.Volume != 1 {
-		streamer = &effects.Volume{
-			Streamer: streamer,
-			Base:     10,
-			Volume:   entry.Volume - 1,
-		}
-	}
-
-	if format.NumChannels < 2 {
-		// Fix mono streams playing at 2x speed
-		streamer = beep.ResampleRatio(3, 0.5, streamer)
 	}
 
 	mu.Lock()
 	defer mu.Unlock()
-	*stream = append(*stream, Streamer{
-		Streamer: streamer,
-		Closer:   closer,
-		Format:   format,
-	})
+	*stream = append(*stream, streamer)
 	return nil
 }
 
