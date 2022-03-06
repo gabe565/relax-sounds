048d979f4d37295fb04dd96aa860a5a59e02d012
Gabe Cook <gabe565@gmail.com>
2022-03-06 17:38:49 -0600

    :sparkles: Sort sounds response

--- internal/sound/sounds.go
+++ internal/sound/sounds.go
@@ -5,8 +5,13 @@ import (
 	"golang.org/x/sync/errgroup"
 	"io/fs"
 	"log"
+	"regexp"
+	"sort"
+	"strconv"
 	"sync"
 )
+
+var sortMixed = regexp.MustCompile("^[0-9]+")
 
 func LoadAll(fsys fs.FS) (sounds []Sound, err error) {
 	var mu sync.Mutex
@@ -40,5 +45,24 @@ func LoadAll(fsys fs.FS) (sounds []Sound, err error) {
 	})
 
 	err = group.Wait()
+
+	// Sort numerically if possible. Falls back to string sort
+	sort.Slice(sounds, func(i, j int) bool {
+		left := sounds[i].Id
+		right := sounds[j].Id
+
+		if leftInt, err := strconv.Atoi(sortMixed.FindString(left)); err == nil {
+			if rightInt, err := strconv.Atoi(sortMixed.FindString(right)); err == nil {
+				if leftInt != rightInt {
+					// Compare ints
+					return leftInt < rightInt
+				}
+			}
+		}
+
+		// Compare strings
+		return left < right
+	})
+
 	return sounds, err
 }
