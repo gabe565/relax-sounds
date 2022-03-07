b7f31082d51c40e9607bf9e3013df4aa85015ad5
Gabe Cook <gabe565@gmail.com>
2022-03-06 20:47:12 -0600

    :bug: Add workaround for __onGCastApiAvailable called before window.cast is set

--- frontend/src/cast.js
+++ frontend/src/cast.js
@@ -1,7 +1,17 @@
 import store from './plugins/store/main';
+import { wait } from './util/helpers';
 
-window.__onGCastApiAvailable = (isAvailable) => {
+window.__onGCastApiAvailable = async (isAvailable) => {
   if (isAvailable) {
-    store.dispatch('player/initializeCastApi');
+    // Workaround for __onGCastApiAvailable called before window.cast is set
+    let waitMs = 100;
+    while (!window.cast) {
+      console.warn(`Cast is undefined. Retrying setup in ${waitMs}ms.`);
+      // eslint-disable-next-line no-await-in-loop
+      await wait(waitMs);
+      waitMs *= 2;
+    }
+
+    await store.dispatch('player/initializeCastApi');
   }
 };
