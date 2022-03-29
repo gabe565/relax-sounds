bd46fd67813c006ad596ffcf1626ad73843a9fd7
Gabe Cook <gabe565@gmail.com>
2022-03-29 00:47:35 -0500

    :zap: Turns out, it's faster to not use a function variable

--- internal/encoder/encode.go
+++ internal/encoder/encode.go
@@ -26,16 +26,6 @@ func Encode(ctx context.Context, w io.Writer, s beep.Streamer, format beep.Forma
 	samples := make([][2]float64, 512)
 	buffer := make([]byte, len(samples)*format.Width())
 
-	var encode func([]byte, [2]float64) int
-	switch format.Precision {
-	case 1:
-		encode = format.EncodeUnsigned
-	case 2, 3:
-		encode = format.EncodeSigned
-	default:
-		return fmt.Errorf("encode: invalid precision: %d", format.Precision)
-	}
-
 	for {
 		if ctx.Err() != nil {
 			return ctx.Err()
@@ -47,8 +37,17 @@ func Encode(ctx context.Context, w io.Writer, s beep.Streamer, format beep.Forma
 		}
 
 		buf := buffer
-		for _, sample := range samples[:n] {
-			buf = buf[encode(buf, sample):]
+		switch format.Precision {
+		case 1:
+			for _, sample := range samples[:n] {
+				buf = buf[format.EncodeUnsigned(buf, sample):]
+			}
+		case 2, 3:
+			for _, sample := range samples[:n] {
+				buf = buf[format.EncodeSigned(buf, sample):]
+			}
+		default:
+			return fmt.Errorf("encode: invalid precision: %d", format.Precision)
 		}
 
 		_, err := w.Write(buffer[:n*format.Width()])
