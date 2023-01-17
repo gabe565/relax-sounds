042112bfb8e67a135845d0ec5dc50f385d40ce38
Gabe Cook <gabe565@gmail.com>
2023-01-17 01:56:57 -0600

    :sparkles: (http): Add graceful shutdown

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"github.com/gabe565/relax-sounds/internal/server"
 	flag "github.com/spf13/pflag"
 	"io/fs"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
+	"time"
 )
 
 const EnvPrefix = "RELAX_SOUNDS_"
@@ -41,11 +46,44 @@ func main() {
 			panic(err)
 		}
 	}
-	router := server.Setup(frontendFs, os.DirFS(*dataDir))
+
+	server := &http.Server{
+		Addr:    *address,
+		Handler: server.Setup(frontendFs, os.DirFS(*dataDir)),
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		<-sig
+
+		// Shutdown signal with grace period of 60 seconds
+		ctx, cancelTimeout := context.WithTimeout(ctx, 60*time.Second)
+		defer func() {
+			cancelTimeout()
+		}()
+
+		// Trigger graceful shutdown
+		log.Println("Performing graceful shutdown...")
+		if err := server.Shutdown(ctx); err != nil {
+			if errors.Is(ctx.Err(), context.DeadlineExceeded) {
+				log.Println("Graceful shutdown timed out")
+			} else {
+				log.Println(err)
+			}
+		}
+		cancel()
+	}()
 
 	log.Println("Listening on " + *address)
-	err = http.ListenAndServe(*address, router)
-	if err != nil {
-		log.Fatalln(err)
+	err = server.ListenAndServe()
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Fatal(err)
 	}
+
+	// Wait for server context to be stopped
+	<-ctx.Done()
+	log.Println("Exiting")
 }
