093b204c39785b43a3d1e044ebb4d7b05531598e
Gabe Cook <gabe565@gmail.com>
2023-01-18 03:30:08 -0600

    :rotating_light: (lint): Lint Vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,65 +1,62 @@
-import vue from '@vitejs/plugin-vue';
-import vuetify from 'vite-plugin-vuetify';
-import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
+import vue from "@vitejs/plugin-vue";
+import vuetify from "vite-plugin-vuetify";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     vue(),
     vuetify({
       styles: {
-        configFile: 'src/scss/variables.scss',
+        configFile: "src/scss/variables.scss",
       },
     }),
     VitePWA({
       devOptions: {
         enabled: true,
       },
-      includeAssets: [
-        'favicon.ico',
-        'img/icons/*',
-      ],
+      includeAssets: ["favicon.ico", "img/icons/*"],
       manifest: {
-        name: 'Relax Sounds',
-        short_name: 'Relax Sounds',
-        id: '/',
-        description: 'Stream relaxing sound mixes',
-        theme_color: '#673AB7',
-        background_color: '#673AB7',
+        name: "Relax Sounds",
+        short_name: "Relax Sounds",
+        id: "/",
+        description: "Stream relaxing sound mixes",
+        theme_color: "#673AB7",
+        background_color: "#673AB7",
         icons: [
           {
-            src: 'img/icons/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
+            src: "img/icons/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'img/icons/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
+            src: "img/icons/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
           },
           {
-            src: 'img/icons/android-chrome-maskable-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'maskable',
+            src: "img/icons/android-chrome-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
           },
           {
-            src: 'img/icons/android-chrome-maskable-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
+            src: "img/icons/android-chrome-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
           },
         ],
       },
       workbox: {
         clientsClaim: true,
-        globPatterns: ['**/*{js,css,html,woff2}'],
+        globPatterns: ["**/*{js,css,html,woff2}"],
         runtimeCaching: [
           {
             urlPattern: /\/api\//,
-            handler: 'NetworkFirst',
+            handler: "NetworkFirst",
             options: {
-              cacheName: 'api-cache',
+              cacheName: "api-cache",
               cacheableResponse: {
                 statuses: [0, 200],
               },
@@ -67,9 +64,9 @@ export default defineConfig({
           },
           {
             urlPattern: /\/data\/audio/,
-            handler: 'CacheFirst',
+            handler: "CacheFirst",
             options: {
-              cacheName: 'data-cache',
+              cacheName: "data-cache",
               expiration: {
                 maxAgeSeconds: 60 * 60 * 24 * 31,
               },
