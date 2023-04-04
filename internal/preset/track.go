4592618708f6781f71fc0be356b39228f677a2a1
Gabe Cook <gabe565@gmail.com>
2023-04-04 16:56:41 -0500

    :fire: (preset): Remove path receiver func

--- internal/preset/track.go
+++ internal/preset/track.go
@@ -1,7 +1,5 @@
 package preset
 
-import "path/filepath"
-
 type Track struct {
 	Id     string
 	Volume float64
@@ -9,8 +7,4 @@ type Track struct {
 
 func (track Track) ToShorthand() ShorthandTrack {
 	return ShorthandTrack{track.Id, track.Volume}
-}
-
-func (track Track) Path() string {
-	return filepath.Join("/", "audio", track.Id+".ogg")[1:]
 }
