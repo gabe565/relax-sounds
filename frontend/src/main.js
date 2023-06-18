b8cee9ac9e67f87e807aa51793602cfe3fc6c89c
Gabe Cook <gabe565@gmail.com>
2023-06-18 16:07:05 -0500

    :heavy_minus_sign: (frontend): Remove FontAwesome Pro

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,6 +7,4 @@ import App from "./App.vue";
 import "./cast";
 import "./plugins/buffer";
 
-import("./scss/fontawesome.scss");
-
 createApp(App).use(createPinia()).use(vuetify).use(router).mount("#app");
