32c24da307eeeed8a3acf35348b2efb163fe1a21
Gabe Cook <gabe565@gmail.com>
2023-06-19 01:53:40 -0500

    :recycle: (sounds): Create components for each sound card button

--- frontend/src/components/Sounds/SoundCard.vue
+++ frontend/src/components/Sounds/SoundCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card variant="outlined" :dark="showProgress" class="px-1 py-2">
+  <v-card variant="outlined" class="px-1 py-2">
     <div class="d-flex flex-row flex-nowrap align-center">
       <div class="text-truncate flex-grow-1">
         <v-card-title class="text-h5">
@@ -10,38 +10,8 @@
         </v-card-title>
       </div>
 
-      <v-dialog v-if="showProgress" location="bottom" location-strategy="connected" max-width="400">
-        <template #activator="{ props }">
-          <v-btn v-bind="props" elevation="0" icon variant="plain" aria-label="Volume">
-            <v-icon :icon="VolumeIcon" aria-hidden="true" />
-          </v-btn>
-        </template>
-
-        <v-card class="pa-8">
-          <v-slider
-            v-model="volumePercentage"
-            :min="0"
-            :max="100"
-            :step="1"
-            thumb-label
-            color="deep-orange-lighten-1"
-            class="pb-1"
-            hide-details
-          />
-        </v-card>
-      </v-dialog>
-
-      <v-btn
-        elevation="0"
-        icon
-        variant="plain"
-        :aria-label="sound.isPlaying ? 'Stop' : 'Play'"
-        :loading="sound.isLoading"
-        @click.stop="playStop"
-      >
-        <v-icon v-if="sound.isPlaying" :icon="PauseIcon" aria-hidden="true" size="large" />
-        <v-icon v-else :icon="PlayIcon" aria-hidden="true" size="x-large" />
-      </v-btn>
+      <volume-button :sound="sound" :show="showVolume" />
+      <play-pause-button :sound="sound" />
     </div>
   </v-card>
 </template>
@@ -49,10 +19,8 @@
 <script setup>
 import { computed } from "vue";
 import { Icon } from "@iconify/vue";
-import PlayIcon from "~icons/material-symbols/play-arrow-rounded";
-import PauseIcon from "~icons/material-symbols/pause-rounded";
-import VolumeIcon from "~icons/material-symbols/volume-up-rounded";
-import { usePlayerStore } from "../../plugins/store/player";
+import PlayPauseButton from "./Buttons/PlayPauseButton.vue";
+import VolumeButton from "./Buttons/VolumeButton.vue";
 
 const props = defineProps({
   sound: {
@@ -61,22 +29,7 @@ const props = defineProps({
   },
 });
 
-const volumePercentage = computed({
-  get() {
-    return props.sound.volume * 100;
-  },
-  set(newValue) {
-    // eslint-disable-next-line vue/no-mutating-props
-    props.sound.volume = newValue / 100;
-    usePlayerStore().updateCast();
-  },
-});
-
 const iconColor = computed(() => (props.sound.isStopped ? "" : "primary"));
 
-const showProgress = computed(() => !props.sound.isStopped);
-
-const playStop = async () => {
-  return usePlayerStore().playStop({ sound: props.sound });
-};
+const showVolume = computed(() => !props.sound.isStopped);
 </script>
