ff3f438e61d983399d06307e71e6d65b9f0dd740
Gabe Cook <gabe565@gmail.com>
2023-07-26 00:22:57 -0500

    :zap: (stream): Load all sounds with a single SQL query

--- internal/stream/new.go
+++ internal/stream/new.go
@@ -1,14 +1,19 @@
 package stream
 
 import (
+	"errors"
+	"fmt"
 	"io/fs"
 	"path/filepath"
 	"sync"
 
 	"github.com/gabe565/relax-sounds/internal/preset"
 	"github.com/pocketbase/pocketbase/daos"
+	"github.com/pocketbase/pocketbase/models"
 	"golang.org/x/sync/errgroup"
 )
+
+var ErrInvalidRecordId = errors.New("invalid record ID")
 
 func New(dataDir fs.FS, dao *daos.Dao, p preset.Preset) (stream Streams, err error) {
 	s := make(Streams, 0, len(p.Tracks))
@@ -17,14 +22,30 @@ func New(dataDir fs.FS, dao *daos.Dao, p preset.Preset) (stream Streams, err err
 	group := errgroup.Group{}
 	group.SetLimit(5)
 
+	ids := make([]string, 0, len(p.Tracks))
+	for _, entry := range p.Tracks {
+		ids = append(ids, entry.Id)
+	}
+	records, err := dao.FindRecordsByIds("sounds", ids)
+	if err != nil {
+		return s, err
+	}
+
 	for _, entry := range p.Tracks {
 		entry := entry
-		group.Go(func() error {
-			record, err := dao.FindRecordById("sounds", entry.Id)
-			if err != nil {
-				return err
-			}
 
+		var record *models.Record
+		for _, v := range records {
+			if v.GetId() == entry.Id {
+				record = v
+				break
+			}
+		}
+		if record == nil {
+			return s, fmt.Errorf("%w: %s", ErrInvalidRecordId, entry.Id)
+		}
+
+		group.Go(func() error {
 			path := filepath.Join(record.BaseFilesPath(), record.Get("file").(string))
 
 			f, err := dataDir.Open(path)
