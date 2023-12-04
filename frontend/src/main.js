96e48f543f9fa52fe633d8dd8f54b81a98d5172b
Gabe Cook <gabe565@gmail.com>
2023-12-04 14:39:47 -0600

    style(toast): Move to top right and fix mobile overlap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -14,7 +14,8 @@ const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
 
 const toastOptions = {
-  position: POSITION.BOTTOM_RIGHT,
+  position: POSITION.TOP_RIGHT,
+  containerClassName: "mt-16 pt-3 mt-sm-0 pt-sm-0",
   showCloseButtonOnHover: true,
   transition: "Vue-Toastification__fade",
 };
