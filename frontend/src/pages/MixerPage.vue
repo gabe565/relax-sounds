2a3df0bd7a39e04943e950ff3409c2db852e409e
Gabe Cook <gabe565@gmail.com>
2023-12-04 14:08:40 -0600

    fix(mixer): Load sounds on init

--- frontend/src/pages/MixerPage.vue
+++ frontend/src/pages/MixerPage.vue
@@ -1,21 +1,28 @@
 <template>
   <PageLayout>
     <v-row>
-      <v-fade-transition group leave-absolute>
-        <v-col
-          v-for="sound of player.soundsNotStopped"
-          :key="sound.id"
-          cols="12"
-          sm="6"
-          md="4"
-          xl="3"
-        >
-          <MixerCard :sound="sound" />
-        </v-col>
-        <v-col v-if="player.soundsNotStopped.length === 0">
-          <v-alert prominent text type="info">No sounds are playing</v-alert>
-        </v-col>
-      </v-fade-transition>
+      <template v-if="loading">
+        <v-overlay v-model="loading" class="align-center justify-center" persistent>
+          <v-progress-circular color="primary" indeterminate size="64" />
+        </v-overlay>
+      </template>
+      <template v-else>
+        <v-fade-transition group leave-absolute>
+          <v-col
+            v-for="sound of player.soundsNotStopped"
+            :key="sound.id"
+            cols="12"
+            sm="6"
+            md="4"
+            xl="3"
+          >
+            <MixerCard :sound="sound" />
+          </v-col>
+          <v-col v-if="player.soundsNotStopped.length === 0">
+            <v-alert prominent text type="info">No sounds are playing</v-alert>
+          </v-col>
+        </v-fade-transition>
+      </template>
     </v-row>
   </PageLayout>
 </template>
@@ -24,6 +31,21 @@
 import PageLayout from "../layouts/PageLayout.vue";
 import MixerCard from "../components/Mixer/MixerCard.vue";
 import { usePlayerStore } from "../plugins/store/player";
+import { onMounted, ref } from "vue";
+import { useToast } from "vue-toastification";
 
 const player = usePlayerStore();
+const loading = ref(true);
+const toast = useToast();
+
+onMounted(async () => {
+  try {
+    await usePlayerStore().initSounds();
+  } catch (err) {
+    console.error(err);
+    toast.error("Failed to fetch sounds.");
+  } finally {
+    loading.value = false;
+  }
+});
 </script>
