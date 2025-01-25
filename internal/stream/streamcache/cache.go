1975df078957ccda83b66d2ea9a96d7dce78baf8
Gabe Cook <gabe565@gmail.com>
2025-01-25 02:13:38 -0600

    fix(streamcache): Fix cron cleanup

--- internal/stream/streamcache/cache.go
+++ internal/stream/streamcache/cache.go
@@ -1,27 +1,32 @@
 package streamcache
 
 import (
+	"strconv"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"gabe565.com/relax-sounds/internal/config"
 	"gabe565.com/utils/must"
-	"github.com/pocketbase/pocketbase"
 )
+
+//nolint:gochecknoglobals
+var nextID atomic.Int64
 
 type Cache struct {
 	conf    *config.Config
+	id      int64
 	entries map[string]*Entry
-	close   chan<- struct{}
 	mu      sync.Mutex
 }
 
 func New(conf *config.Config) *Cache {
 	cache := &Cache{
 		conf:    conf,
+		id:      nextID.Add(1),
 		entries: make(map[string]*Entry),
 	}
-	must.Must(cache.RegisterCron(conf.App, ""))
+	must.Must(cache.RegisterCron())
 	return cache
 }
 
@@ -65,13 +70,21 @@ func (a *Cache) Has(id string) bool {
 	return ok
 }
 
+func (a *Cache) idString() string {
+	var id string
+	if a.id != 1 {
+		id = strconv.FormatInt(a.id, 10)
+	}
+	return id
+}
+
 func (a *Cache) Close() {
-	a.close <- struct{}{}
+	a.conf.App.Cron().Remove("mixStreamCleanup" + a.idString())
 	a.cleanup(0)
 }
 
-func (a *Cache) RegisterCron(app *pocketbase.PocketBase, id string) error {
-	return app.Cron().Add("mixStreamCleanup"+id, "* * * * *", func() {
+func (a *Cache) RegisterCron() error {
+	return a.conf.App.Cron().Add("mixStreamCleanup"+a.idString(), "* * * * *", func() {
 		a.cleanup(a.conf.CacheCleanAfter)
 	})
 }
