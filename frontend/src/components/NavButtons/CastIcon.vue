6659a77692b8bbc9b444b9049243b98f2d63db9b
Gabe Cook <git@gabe565.com>
2026-07-03 03:56:56 -0500

    fix: only show Cast button/tooltip when icon is shown

--- frontend/src/components/NavButtons/CastIcon.vue
+++ frontend/src/components/NavButtons/CastIcon.vue
@@ -1,9 +1,9 @@
 <template>
   <v-tooltip v-if="player.castEnabled" text="Cast" :location="tooltipLocation">
     <template #activator="{ props }">
-      <v-btn v-bind="props" icon title="Cast" aria-label="Cast">
+      <v-btn v-show="show" v-bind="props" icon title="Cast" aria-label="Cast">
         <v-icon>
-          <google-cast-launcher class="absolute inset-0 transition-colors" />
+          <google-cast-launcher ref="launcherRef" class="absolute inset-0 transition-colors" />
         </v-icon>
       </v-btn>
     </template>
@@ -11,6 +11,8 @@
 </template>
 
 <script setup>
+import { useMutationObserver } from "@vueuse/core";
+import { ref, useTemplateRef, watch } from "vue";
 import { usePlayer } from "@/plugins/store/player";
 
 defineProps({
@@ -21,6 +23,15 @@ defineProps({
 });
 
 const player = usePlayer();
+const launcherRef = useTemplateRef("launcherRef");
+const show = ref(false);
+
+const update = () => {
+  show.value = launcherRef.value ? getComputedStyle(launcherRef.value).display !== "none" : false;
+};
+
+watch(launcherRef, update, { once: true });
+useMutationObserver(launcherRef, update, { attributes: true });
 </script>
 
 <style>
